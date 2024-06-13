073cb6e012861a19240a6100ee2ffb170999501b
skandgt <skandgt989@gmail.com>
2024-06-13 21:15:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -119,3 +119,10 @@ app.put("/api/delete_product", (req, res) => {
         });
     }
 });
+
+app.get("/api/get_image", (req, res) => {
+    res.status(200).send({
+        "status": "200",
+        "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fflowers%2F&psig=AOvVaw3BIgfp58MTJ7K0Qqin_dOd&ust=1718379439842000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNDLvbX02IYDFQAAAAAdAAAAABAE"
+    });
+});
